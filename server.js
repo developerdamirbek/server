718e6693c05ccff77899ed796d23dd1d008c332c
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-03-07 16:37:57 +0500

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const jsonServer = require("json-server");
 const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const customRoutes = require("./routes.json"); // Import custom routes
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -10,6 +11,15 @@ server.use((req, res, next) => {
   setTimeout(next, 700);
 });
 
+// Set up custom routes
+for (let route in customRoutes) {
+  if (customRoutes.hasOwnProperty(route)) {
+    server.use(route, (req, res, next) => {
+      setTimeout(next, customRoutes[route]);
+    });
+  }
+}
+
 setTimeout(() => {
   server.use(router);
   app.use(server);
@@ -17,8 +27,8 @@ setTimeout(() => {
 
 const app = express();
 
-const PORT = 3600;
+const PORT = 3601;
 
 app.listen(PORT, () => {
-  console.log("Server is running on...");
+  console.log("Server is running on port " + PORT);
 });
